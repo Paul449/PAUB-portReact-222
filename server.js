82e6542ab1cc5ee55a1b01e10d4c67caefca258a
paul449 <paul_bilbatua@hotmail.com>
2024-08-05 11:51:44 -0500

    new

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 const App = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 //middleware
