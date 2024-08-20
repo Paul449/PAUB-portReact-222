af4095cbe39d316a271190667839220f01ef0a17
paul449 <paul_bilbatua@hotmail.com>
2024-08-20 11:22:50 -0500

    css

--- src/App.jsx
+++ src/App.jsx
@@ -7,49 +7,52 @@ import Portfolio from './components/Portfolio';
 import Resume from './components/Resume';
 import './App.css'
 function App() {
-  //use state for current tab
- const [Tab, setTab] = useState(0)
-// conditional statements
-function Tabs(){
-  if(Tab === 'AboutMe'){
-    return <AboutMe/>
-  } else if(Tab === 'Contact'){
-    return <Contact/>
-  } else if(Tab === 'Portfolio'){
-    return <Portfolio/>
-  } 
-    return <Resume/>
-  
-}
-  return(<>
-<div className='wrapper'>
+  const [Tab, setTab] = useState('AboutMe');
+
+  const renderContent = () => {
+    switch (Tab) {
+      case 'AboutMe':
+        return <AboutMe />;
+      case 'Contact':
+        return <Contact />;
+      case 'Portfolio':
+        return <Portfolio />;
+      case 'Resume':
+        return <Resume />;
+      default:
+        return <AboutMe />;
+    }
+  };
+
+  return (
+    <div className='wrapper'>
       <header>
         <ul className='nav-list'>
-        <li id='One'><button id='AM' onClick={()=> setTab('AboutMe')}>About Me</button></li>
-        <li id='Two'><button id='C' onClick={()=> setTab('Contact')}>Contact</button></li>
-        <li id='Three'><button id='P' onClick={()=> setTab('Portfolio')}>Portfolio</button></li>
-        <li id='Fourth'><button id='R' onClick={()=> setTab('Resume')}>Resume</button></li>
+          <li><button onClick={() => setTab('AboutMe')}>About Me</button></li>
+          <li><button onClick={() => setTab('Contact')}>Contact</button></li>
+          <li><button onClick={() => setTab('Portfolio')}>Portfolio</button></li>
+          <li><button onClick={() => setTab('Resume')}>Resume</button></li>
         </ul>
       </header>
       <div className='content'>
-        {Tabs()}
+        {renderContent()}
       </div>
       <footer>
-      <a href='https://github.com/Paul449'>
-      <img id='G' width={50 + 'px'} height={50 + 'px'} src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg' alt='github logo'></img>
-      </a>
-      <a href='https://www.linkedin.com/in/paul-bilbatua-6b4876232/'>
-      <img id='L' width={50 + 'px'} height={50 + 'px'} src='https://www.svgrepo.com/show/922/linkedin.svg' alt='linkedlin logo'></img>
-      </a>
-      <a href='https://stackoverflow.com/users/23615930/paul-biro'>
-      <img id='S' width={50 + 'px'} height={50 + 'px'} src='https://upload.wikimedia.org/wikipedia/commons/e/ef/Stack_Overflow_icon.svg' alt='stackoverflow'></img>
-      </a>
+        <a href='https://github.com/Paul449'>
+          <img width={50} height={50} src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg' alt='GitHub logo' />
+        </a>
+        <a href='https://www.linkedin.com/in/paul-bilbatua-6b4876232/'>
+          <img width={50} height={50} src='https://www.svgrepo.com/show/922/linkedin.svg' alt='LinkedIn logo' />
+        </a>
+        <a href='https://stackoverflow.com/users/23615930/paul-biro'>
+          <img width={50} height={50} src='https://upload.wikimedia.org/wikipedia/commons/e/ef/Stack_Overflow_icon.svg' alt='Stack Overflow logo' />
+        </a>
       </footer>
     </div>
-  </>)
+  );
 }
 
-export default App
+export default App;
 /*
  <img src ="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt='github icon' width={em}/>
       <img src="https://www.svgrepo.com/show/922/linkedin.svg" alt='linkedlin icon' />
