c173d94bcdbb62270342714330dcac66880e7f57
paul449 <paul_bilbatua@hotmail.com>
2025-02-10 18:22:33 -0600

    pdf

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,11 +1,30 @@
-import React, { useEffect } from "react";
+import { pdfjs } from "react-pdf";
+import React, { useEffect, useState } from "react";
 //function
 export default function Resume(){
-   
+   const [download,setDownload] = useState(false);
+   useEffect(() => {
+    const pdfPath = "/src/Paul-Bilbatua-Resume.pdf";
+
+    pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+    pdfjs.getDocument(pdfPath).promise
+        .then((pdf) => {
+            console.log("PDF loaded", pdf);
+        })
+        .catch((error) => {
+            console.error("Error loading PDF", error);
+        });
+}, []);
+
+const handleDownload = () => {
+    window.open("/src/Paul-Bilbatua-Resume.pdf", "_blank");
+    setDownload(true);
+};
     return(<>
     <div>
         <h2 id="resume-title">Resume</h2>
-        <p id="download">Download my <button id="resumebtn">resume</button></p>
+        <p id="download">Download my <button onClick={handleDownload} id="resumebtn">resume</button></p>
     </div>
     <div className="front-end">
         <h2>Front End Proficiencies</h2>
