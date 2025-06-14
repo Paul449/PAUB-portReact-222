1cac63bccce2babee1a9bc2c7841f3b24742e102
paul449 <paul_bilbatua@hotmail.com>
2025-06-14 18:02:13 -0500

    vote4goat link

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -66,7 +66,7 @@ export default function Portfolio() {
             title: "Vote4Goat",
             description: "Fantasy basketball voting application where users can vote for their favorite players and view real-time voting results.",
             technologies: ["MERN Stack", "MongoDB", "Express", "React","Node.js"],
-            liveUrl: "#", // Add live URL if available
+            liveUrl: "https://vote4goat.netlify.app/", // Add live URL if available
             githubUrl: "https://github.com/Paul449/vote4Goat", // Add your actual GitHub URL
             image: "./assets/Screenshot%202024-07-23%20200824.png",
             category: "Full Stack Application"
